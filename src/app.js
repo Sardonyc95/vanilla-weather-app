0b9827aeadf20b01c88b4d9bfc878e426328a598
Amarachi Victoria Adolphus <111003235+Sardonyc95@users.noreply.github.com>
2023-04-02 19:33:45 +0100

    added the search engine

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ function formateDate(timestamp) {
   }
   let minutes = date.getMinutes();
   if (minutes < 10) {
-    minutes = `0${hours}`;
+    minutes = `0${minutes}`;
   }
 
   return `${day} ${hours}:${minutes}`;
@@ -56,9 +56,34 @@ function displayTemperature(response) {
   wind.innerHTML = Math.round(response.data.wind.speed);
 }
 
-let apikey = "caa883a4a60d93878755b08a933f74ea";
-let unit = "metric";
-let city = "Owerri";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=${unit}`;
+function searchCity(value) {
+  let apikey = "caa883a4a60d93878755b08a933f74ea";
+  let unit = "metric";
+  let city = value;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=${unit}`;
 
-axios.get(apiUrl).then(displayTemperature);
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function inputSubmit(event) {
+  event.preventDefault();
+
+  let formInput = document.querySelector("#searchBtn");
+  searchCity(formInput.value);
+}
+
+function showFahrenheitConversion(event) {
+  event.preventDefault();
+
+  // let celsiusTemperature = document.querySelector("celsiusLink");
+  let fahrenheitTemperature = document.querySelector("tempDegree");
+  fahrenheitTemperature.innerHTML = (30 * 9) / 5 + 32;
+}
+
+let form = document.querySelector("#submit");
+form.addEventListener("submit", inputSubmit);
+
+let fahrenheit = document.querySelector("#fahrenheitLink");
+fahrenheit.addEventListener("click", showFahrenheitConversion);
+
+searchCity("Owerri");
